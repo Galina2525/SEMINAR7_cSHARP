b8b49b4d91aa92ff4b39e1b44fa406b337a4f393
Galina <koroleva2525.gk@gmail.com>
2022-11-28 09:34:46 +0100

    add task46

---
+++ Task46/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размером m x n, заполненный случайными целыми числами
+//m =3, n =4
+//1 4 8 1
+//5 -2 33 -2
+//77 3 8 1
+
+Console.WriteLine("Enter m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] ourMatrix = GetMatrix(m, n, -10, 10);
+
+//Console.WriteLine(string.Join(" ,  ", ourMatrix));//этот метод не работает
+
+
+int [,] GetMatrix(int rowsCount, int columnsCount, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random random = new Random();
+    for( int i = 0; i < matrix.GetLength(0); i ++)
+    {
+       for ( int j = 0; j < matrix.GetLength(1); j ++)
+       {
+        matrix[i,j] = random.Next(minValue, maxValue);
+       }
+    }
+    return matrix;
+}
+PrintMatrix(ourMatrix);
+
+void PrintMatrix(int[,] matrix)
+{
+      for( int i = 0; i < matrix.GetLength(0); i ++)
+    {
+       for ( int j = 0; j < matrix.GetLength(1); j ++)
+       {
+        Console.Write(matrix[i, j] + " ");
+       }
+       Console.WriteLine();
+    } 
+}
